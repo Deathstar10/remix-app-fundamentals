9d06c7445c257dc5ee2c3dc972f82c441bddef9f
Samala <asamala@deloitte.com>
2024-05-02 16:54:13 +0530

    fix 2

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -49,6 +49,7 @@ export default function Contact() {
           <Form action="edit">
             <button type="submit">Edit</button>
             <p>hello 1</p>
+            <p>hello 2</p>
           </Form>
 
           <Form
